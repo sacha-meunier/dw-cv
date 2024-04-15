d0a5ba5d83ab3792ba9e6c78789f6dbf30ad930f
Sacha Meunier <sacha.meunier@student.hepl.be>
2024-04-15 09:46:32 +0200

    Update index.php

--- index.php
+++ index.php
@@ -8,13 +8,16 @@
 
     <link rel="stylesheet" href="dist/css/main.css">
     <meta name="author" content="Sacha Meunier">
-    <meta name="description" content="Bienvenue sur mon CV en ligne en tant que développeur Web. Découvrez mon parcours professionnel, ma formation, mes compétences et mes réalisations. Consultez mon site pour en savoir plus.">
-    <meta name="keywords" content="développement web, développeur web, développeur front-end, développeur back-end, développement d’applications web, UX, UI, UX/UI, HTML, CSS, JavaScript, PHP, Git, programmation">
+    <meta name="description"
+          content="Bienvenue sur mon CV en ligne en tant que développeur Web. Découvrez mon parcours professionnel, ma formation, mes compétences et mes réalisations. Consultez mon site pour en savoir plus.">
+    <meta name="keywords"
+          content="développement web, développeur web, développeur front-end, développeur back-end, développement d’applications web, UX, UI, UX/UI, HTML, CSS, JavaScript, PHP, Git, programmation">
 
     <meta property="og:url" content="https://sachameunier-cv.com">
     <meta property="og:title" content="CV - Sacha Meunier">
     <meta property="og:type" content="website">
-    <meta property="og:description" content="Bienvenue sur mon CV en ligne en tant que développeur Web. Découvrez mon parcours professionnel, ma formation, mes compétences et mes réalisations. Consultez mon site pour en savoir plus.">
+    <meta property="og:description"
+          content="Bienvenue sur mon CV en ligne en tant que développeur Web. Découvrez mon parcours professionnel, ma formation, mes compétences et mes réalisations. Consultez mon site pour en savoir plus.">
     <meta property="og:image" content="/content/images/logo-menu.svg">
     <meta property="og:image:alt" content="Le logo de Sacha Meunier"/>
 
@@ -23,8 +26,6 @@
 </head>
 <body>
 <main>
-    <?php /*include 'header.php';*/?>
-    <?php /*include 'footer.php';*/?>
 </main>
 <script src="dist/js/main.js"></script>
 </body>
