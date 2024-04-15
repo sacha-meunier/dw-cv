63ab648b37306508e476ef0c7d63a7d5e9b6157e
Sacha Meunier <sacha.meunier@student.hepl.be>
2024-04-15 09:05:51 +0200

    🎉 Initial commit

---
+++ index.php
@@ -0,0 +1,31 @@
+<!doctype html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <title>CV - Sacha Meunier</title>
+
+    <link rel="stylesheet" href="dist/css/main.css">
+    <meta name="author" content="Sacha Meunier">
+    <meta name="description" content="Bienvenue sur mon CV en ligne en tant que développeur Web. Découvrez mon parcours professionnel, ma formation, mes compétences et mes réalisations. Consultez mon site pour en savoir plus.">
+    <meta name="keywords" content="développement web, développeur web, développeur front-end, développeur back-end, développement d’applications web, UX, UI, UX/UI, HTML, CSS, JavaScript, PHP, Git, programmation">
+
+    <meta property="og:url" content="https://sachameunier-cv.com">
+    <meta property="og:title" content="CV - Sacha Meunier">
+    <meta property="og:type" content="website">
+    <meta property="og:description" content="Bienvenue sur mon CV en ligne en tant que développeur Web. Découvrez mon parcours professionnel, ma formation, mes compétences et mes réalisations. Consultez mon site pour en savoir plus.">
+    <meta property="og:image" content="/content/images/logo-menu.svg">
+    <meta property="og:image:alt" content="Le logo de Sacha Meunier"/>
+
+    <!--<link rel="icon" type="image/svg+xml" href="./favicon.svg"/>-->
+    <!--<link rel="alternate icon" href="/favicon.ico"/>-->
+</head>
+<body>
+<main>
+    <?php /*include 'header.php';*/?>
+    <?php /*include 'footer.php';*/?>
+</main>
+<script src="dist/js/main.js"></script>
+</body>
+</html>
