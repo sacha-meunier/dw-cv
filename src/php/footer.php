0aca9d5bea4bf82b9f978511a83b2f0e158793d5
Sacha Meunier <sacha.meunier@student.hepl.be>
2024-04-19 11:47:02 +0200

    Update footer.php

--- src/php/footer.php
+++ src/php/footer.php
@@ -1,75 +1,92 @@
 <footer class="footer" id="footer">
-    <aside class="footer__about">
-        <h2 class="sro">À propos de moi</h2>
-        <a class="footer__logo" href="/">
-            <img class="footer__logo-image" loading="lazy" src="/content/images/footer-logo.svg" alt="Logo Zekiah"
-                 width="102" height="40">
-        </a>
-        <p class="footer__description">Je crée des sites web au goût du jour, qui fonctionnent sur tous les appareils,
-            pour tous les
-            utilisateurs et qui offrent une expérience satisfaisante.</p>
-        <a class="footer__button" href="#">Discutons</a>
-    </aside>
+    <div class="footer__wrapper">
+        <aside class="footer__about">
+            <h2 class="sro">À propos de moi</h2>
+            <a class="footer__logo" href="/">
+                <img class="footer__logo-image" loading="lazy" src="/content/images/footer-logo.svg" alt="Logo Zekiah"
+                     width="102" height="40">
+            </a>
+            <p class="footer__description">Je crée des sites web au goût du jour, qui fonctionnent sur tous les
+                appareils,
+                pour tous les
+                utilisateurs et qui offrent une expérience satisfaisante.</p>
+            <a class="footer__button" href="#">Discutons</a>
+        </aside>
 
-    <nav class="footer__nav">
-        <h2 class="sro">Navigation</h2>
-        <div class="footer__nav-content">
-            <h3 class="footer__nav-title">Navigation</h3>
-            <ul class="footer__nav-list">
-                <li class="footer__nav-item"><a class="footer__nav-link" href="#">Accueil</a></li>
-                <li class="footer__nav-item"><a class="footer__nav-link" href="#">Expérience</a></li>
-                <li class="footer__nav-item"><a class="footer__nav-link" href="#">Témoignages</a></li>
-                <li class="footer__nav-item"><a class="footer__nav-link" href="#">Formation</a></li>
-                <li class="footer__nav-item"><a class="footer__nav-link" href="#">Compétences</a></li>
-            </ul>
-        </div>
-        <div class="footer__nav-content">
-            <h3 class="footer__nav-title">Ressources</h3>
-            <ul class="footer__nav-list">
-                <li class="footer__nav-item"><a class="footer__nav-link" href="https://www.alsacreations.com/">Alsacréations</a></li>
-                <li class="footer__nav-item"><a class="footer__nav-link" href="https://wakatime.com/">Wakatime</a></li>
-                <li class="footer__nav-item"><a class="footer__nav-link" href="https://www.evernote.design/">Evernote Design</a></li>
-                <li class="footer__nav-item"><a class="footer__nav-link" href="https://www.realtimecolors.com">Realtime Colors</a></li>
-                <li class="footer__nav-item"><a class="footer__nav-link" href="https://www.pillarstack.com/">PillarStack</a></li>
-                <li class="footer__nav-item"><a class="footer__nav-link" href="https://stacksorted.com">Stack Sorted</a></li>
-                <li class="footer__nav-item"><a class="footer__nav-link" href="https://www.raycast.com/">Raycast</a></li>
-                <li class="footer__nav-item"><a class="footer__nav-link" href="https://www.pinterest.fr/">Pinterest</a></li>
-                <li class="footer__nav-item"><a class="footer__nav-link" href="https://dribbble.com">Dribble</a></li>
-                <li class="footer__nav-item"><a class="footer__nav-link" href="https://www.a11yproject.com/">A11Y Project</a></li>
-                <li class="footer__nav-item"><a class="footer__nav-link" href="https://github.com/whitecube/">WhiteCube</a></li>
-            </ul>
-        </div>
-    </nav>
-
-    <section class="footer__social">
-        <h2 class="sro">Mes réseaux sociaux</h2>
-        <ul class="footer__social-list">
-            <li><a href="https://dribbble.com/Zekiahs"><img loading="lazy" src="../../content/images/icon-dribble.svg"
-                                                            alt="Icône de Dribble"></a></li>
-            <li><a href="https://twitter.com/ZekiahVFX"><img loading="lazy" src="../../content/images/icon-twitter.svg"
-                                                             alt="Icône de Twitter"></a></li>
-            <li><a href="https://www.instagram.com/zekiahrr"><img loading="lazy"
-                                                                  src="../../content/images/icon-instagram.svg"
-                                                                  alt="Icône de Instagram"></a></li>
-            <li><a href="https://www.youtube.com/@Zekiah"><img loading="lazy"
-                                                               src="../../content/images/icon-youtube.svg"
-                                                               alt="Icône de Youtube"></a></li>
-            <li><a href="#"><img loading="lazy" src="../../content/images/icon-linkedin.svg"
-                                 alt="Icône de Linkedin"></a></li>
-            <li><a href="#"><img loading="lazy" src="../../content/images/icon-tiktok.svg" alt="Icône de Tiktok"></a>
-            </li>
-        </ul>
-    </section>
-
-    <section class="footer__legal">
-        <h2 class="sro">Mentions légales</h2>
-        <div class="footer__legal-content">
-            <div class="footer__location">
-                <img class="footer__location-icon" loading="lazy" src="../../content/images/icon-location.svg"
-                     alt="Icône de Location" width="18" height="18">
-                <p class="footer__location-text">Faimes, Belgique</p>
+        <nav class="footer__nav">
+            <h2 class="sro">Navigation</h2>
+            <div class="footer__nav-content">
+                <h3 class="footer__nav-title">Navigation</h3>
+                <ul class="footer__nav-list">
+                    <li class="footer__nav-item"><a class="footer__nav-link" href="#">Accueil</a></li>
+                    <li class="footer__nav-item"><a class="footer__nav-link" href="#">Expérience</a></li>
+                    <li class="footer__nav-item"><a class="footer__nav-link" href="#">Témoignages</a></li>
+                    <li class="footer__nav-item"><a class="footer__nav-link" href="#">Formation</a></li>
+                    <li class="footer__nav-item"><a class="footer__nav-link" href="#">Compétences</a></li>
+                </ul>
             </div>
-            <p class="footer__copyright">© 2023 Zekiah. Tous droits réservés.</p>
-        </div>
-    </section>
+            <div class="footer__nav-content">
+                <h3 class="footer__nav-title">Ressources</h3>
+                <ul class="footer__nav-list footer__nav-list--column">
+                    <li class="footer__nav-item"><a class="footer__nav-link" href="https://www.alsacreations.com/">Alsacréations</a>
+                    </li>
+                    <li class="footer__nav-item"><a class="footer__nav-link" href="https://wakatime.com/">Wakatime</a>
+                    </li>
+                    <li class="footer__nav-item"><a class="footer__nav-link" href="https://www.evernote.design/">Evernote
+                            Design</a></li>
+                    <li class="footer__nav-item"><a class="footer__nav-link" href="https://www.realtimecolors.com">Realtime
+                            Colors</a></li>
+                    <li class="footer__nav-item"><a class="footer__nav-link" href="https://www.pillarstack.com/">PillarStack</a>
+                    </li>
+                    <li class="footer__nav-item"><a class="footer__nav-link" href="https://stacksorted.com">Stack
+                            Sorted</a></li>
+                    <li class="footer__nav-item"><a class="footer__nav-link" href="https://www.raycast.com/">Raycast</a>
+                    </li>
+                    <li class="footer__nav-item"><a class="footer__nav-link"
+                                                    href="https://www.pinterest.fr/">Pinterest</a></li>
+                    <li class="footer__nav-item"><a class="footer__nav-link" href="https://dribbble.com">Dribble</a>
+                    </li>
+                    <li class="footer__nav-item"><a class="footer__nav-link" href="https://www.a11yproject.com/">A11Y
+                            Project</a></li>
+                    <li class="footer__nav-item"><a class="footer__nav-link" href="https://github.com/whitecube/">WhiteCube</a>
+                    </li>
+                </ul>
+            </div>
+        </nav>
+
+        <section class="footer__social">
+            <h2 class="sro">Mes réseaux sociaux</h2>
+            <ul class="footer__social-list">
+                <li><a class="footer__social-logo-link" href="https://dribbble.com/Zekiahs"><img class="footer__social-logo-item" loading="lazy"
+                                                                src="../../content/images/icon-dribble.svg"
+                                                                alt="Icône de Dribble" width="24" height="24"></a></li>
+                <li><a class="footer__social-logo-link" href="https://twitter.com/ZekiahVFX"><img class="footer__social-logo-item" loading="lazy"
+                                                                 src="../../content/images/icon-twitter.svg"
+                                                                 alt="Icône de Twitter" width="24" height="24"></a></li>
+                <li><a class="footer__social-logo-link" href="https://www.instagram.com/zekiahrr"><img class="footer__social-logo-item" loading="lazy"
+                                                                      src="../../content/images/icon-instagram.svg"
+                                                                      alt="Icône de Instagram" width="24" height="24"></a></li>
+                <li><a class="footer__social-logo-link" href="https://www.youtube.com/@Zekiah"><img class="footer__social-logo-item" loading="lazy"
+                                                                   src="../../content/images/icon-youtube.svg"
+                                                                   alt="Icône de Youtube" width="24" height="24"></a></li>
+                <li><a class="footer__social-logo-link" href="#"><img class="footer__social-logo-item" loading="lazy" src="../../content/images/icon-linkedin.svg"
+                                     alt="Icône de Linkedin" width="24" height="24"></a></li>
+                <li><a class="footer__social-logo-link" href="#"><img class="footer__social-logo-item" loading="lazy" src="../../content/images/icon-tiktok.svg"
+                                     alt="Icône de Tiktok" width="24" height="24"></a>
+                </li>
+            </ul>
+        </section>
+
+        <section class="footer__legal">
+            <h2 class="sro">Mentions légales</h2>
+            <div class="footer__legal-content">
+                <div class="footer__location">
+                    <img class="footer__location-icon" loading="lazy" src="../../content/images/icon-location.svg"
+                         alt="Icône de Location" width="18" height="18">
+                    <p class="footer__location-text">Faimes, Belgique</p>
+                </div>
+                <p class="footer__copyright">© 2023 Zekiah. Tous droits réservés.</p>
+            </div>
+        </section>
+    </div>
 </footer>
