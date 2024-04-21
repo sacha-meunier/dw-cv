7b729bef177d009d163db4c3b84bb418c7bcaf08
Sacha Meunier <sacha.meunier@student.hepl.be>
2024-04-21 02:40:41 +0200

    Create contact.php

---
+++ src/php/contact.php
@@ -0,0 +1,20 @@
+<section class="contact" id="contact">
+    <div class="contact__wrapper">
+        <h2 class="sro">Contact</h2>
+        <img class="contact__logo" loading="lazy" src="/content/images/logo-low-opacity.svg" alt="Logo">
+        <p class="contact__title">
+            Mon CV vous intéresse ?
+        </p>
+        <p class="contact__title contact__title--primary">Discutons !</p>
+        <p class="contact__description">
+            Vous cherchez un site Web qui vous mettra en avant, fournira une expérience utilisateur agréable
+            et qui plaira grâce à son design riche et tendance. Tout ça rapidement et facilement ?
+        </p>
+        <a class="contact__button" href="#">
+            <span class="contact__button-text">Discutons</span>
+            <div class="contact__button-icon-wrapper"><img class="contact__button-icon"
+                                                           src="/content/images/icon-mail.svg" width="18" height="18"
+                                                           alt="Icône mail"></div>
+        </a>
+    </div>
+</section>
