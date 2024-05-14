0708d09d5e0858cd4258e540eaf3def08ee2f08a
Sacha Meunier <sacha.meunier@student.hepl.be>
2024-05-14 14:58:45 +0200

    Create hobbys.php

---
+++ src/php/hobbys.php
@@ -0,0 +1,45 @@
+<section class="hobbys" id="hobbys">
+    <div class="hobbys__wrapper">
+        <div class="hobbys__content">
+            <h2 class="hobbys__title">
+                <span class="hobbys__title--primary">Les hobbies</span>
+                que j'aime
+            </h2>
+            <p class="hobbys__description">
+                Mes passions sont sources d'inspiration et d'épanouissement qui contribuent à ma motivation et à mes
+                compétences.
+            </p>
+        </div>
+
+        <div class="hobbys__grid">
+            <img
+                    class="hobbys__grid-image"
+                    loading="lazy"
+                    src="/content/images/hobbys-cinema-min.svg"
+                    alt="Icône flèche vers la droite"
+                    width="135"
+                    height="40">
+            <img
+                    class="hobbys__grid-image"
+                    loading="lazy"
+                    src="/content/images/hobbys-coding-min.svg"
+                    alt="Icône flèche vers la droite"
+                    width="135"
+                    height="40">
+            <img
+                    class="hobbys__grid-image"
+                    loading="lazy"
+                    src="/content/images/hobbys-hanging-min.svg"
+                    alt="Icône flèche vers la droite"
+                    width="135"
+                    height="40">
+            <img
+                    class="hobbys__grid-image"
+                    loading="lazy"
+                    src="/content/images/hobbys-learning-min.svg"
+                    alt="Icône flèche vers la droite"
+                    width="135"
+                    height="40">
+        </div>
+    </div>
+</section>
