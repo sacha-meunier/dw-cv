430a4ebdd4645f16c122c7e51c729c2a185f9711
Sacha Meunier <sacha.meunier@student.hepl.be>
2024-05-14 14:58:53 +0200

    Create skills.php

---
+++ src/php/skills.php
@@ -0,0 +1,151 @@
+<!-- TODO adding alt on image -->
+<!-- TODO magnetic hover -->
+<section class="skills">
+    <div class="skills__wrapper">
+
+        <div class="skills__heading">
+            <h2 class="skills__title">
+                <span class="skills__title--primary">Mes</span>
+                Compétences
+            </h2>
+
+            <div class="skills__nav">
+                <a class="skills__button skills__button--text" href="#">
+                    <span class="skills__button-link">Mes compétences</span>
+                </a>
+                <a class="skills__button skills__button--arrow" href="#">
+                    <img class="skills__nav-arrow" src="/content/images/icon-arrow-left-slider.svg"
+                         loading="lazy" alt="Icône flèche vers la gauche"
+                         width="32px" height="32px">
+                </a>
+                <a class="skills__button skills__button--arrow" href="#">
+                    <img class="skills__nav-arrow" src="/content/images/icon-arrow-right-slider.svg"
+                         loading="lazy" alt="Icône flèche vers la gauche"
+                         width="32px" height="32px">
+                </a>
+            </div>
+        </div>
+
+        <div class="skills__carousel">
+            <!-- Card 1 -->
+            <a class="skills__card skills__card--thin skills__card--a" href="#">
+                <div class="skills__card-media">
+                    <img class="skills__card-img" src="" loading="lazy" alt="">
+                </div>
+                <div class="skills__card-content">
+                    <p class="skills__card-title">Développement Web</p>
+                    <p class="skills__card-copy">Sites web axés sur l'expérience et l'accessibilité des
+                        utilisateurs.</p>
+                </div>
+            </a>
+            <!-- Card 2 -->
+            <a class="skills__card skills__card--large skills__card--b" href="#">
+                <div class="skills__card-media">
+                    <img class="skills__card-img" src="" loading="lazy" alt="">
+                </div>
+                <div class="skills__card-content">
+                    <p class="skills__card-title">Créations d'applications</p>
+                    <p class="skills__card-copy">Je rends accessible vos services sous forme d'une application pour tous les appareils.</p>
+                </div>
+            </a>
+            <!-- Card 3 -->
+            <a class="skills__card skills__card--large skills__card--c" href="#">
+                <div class="skills__card-media">
+                    <img class="skills__card-img" src="" loading="lazy" alt="">
+                </div>
+                <div class="skills__card-content">
+                    <p class="skills__card-title">Rebranding</p>
+                    <p class="skills__card-copy">J'améliore votre image de marque actuelle pour favoriser votre expansion.</p>
+                </div>
+            </a>
+            <!-- Card 4 -->
+            <a class="skills__card skills__card--thin skills__card--d" href="#">
+                <div class="skills__card-media">
+                    <img class="skills__card-img" src="" loading="lazy" alt="">
+                </div>
+                <div class="skills__card-content">
+                    <p class="skills__card-title">Gestion de réseaux sociaux</p>
+                    <p class="skills__card-copy">Je développe votre image grâce à l'utilisation des réseaux sociaux.</p>
+                </div>
+            </a>
+            <!-- Card 5 -->
+            <a class="skills__card skills__card--large skills__card--e" href="#">
+                <div class="skills__card-media">
+                    <img class="skills__card-img" src="" loading="lazy" alt="">
+                </div>
+                <div class="skills__card-content">
+                    <p class="skills__card-title">Montage vidéo</p>
+                    <p class="skills__card-copy">Je rends vos contenus vidéo exceptionnels grâce à des montages vivant et dynamique.</p>
+                </div>
+            </a>
+            <!-- Card 6 -->
+            <a class="skills__card skills__card--large skills__card--f" href="#">
+                <div class="skills__card-media">
+                    <img class="skills__card-img" src="" loading="lazy" alt="">
+                </div>
+                <div class="skills__card-content">
+                    <p class="skills__card-title">Photographie</p>
+                    <p class="skills__card-copy">Je prends de magnifiques photos et les retouches pour un résultat hors du commun.</p>
+                </div>
+            </a>
+        </div>
+
+        <div class="skills__slider">
+            <div class="skills__slider-column">
+                <div class="skills__slider-row">
+                    <span class="skills__slider-item">HTML</span>
+                    <span class="skills__slider-item">CSS & SCSS</span>
+                    <span class="skills__slider-item">JavaScript & TypeScript</span>
+                    <span class="skills__slider-item">PHP</span>
+                    <span class="skills__slider-item">MySQL</span>
+                    <span class="skills__slider-item">Python</span>
+                </div>
+                <div class="skills__slider-row">
+                    <span class="skills__slider-item">Figma</span>
+                    <span class="skills__slider-item">Adobe XD</span>
+                    <span class="skills__slider-item">InDesign</span>
+                    <span class="skills__slider-item">Photoshop</span>
+                    <span class="skills__slider-item">Illustrator</span>
+                    <span class="skills__slider-item">After Effects</span>
+                    <span class="skills__slider-item">Première Pro</span>
+                </div>
+                <div class="skills__slider-row">
+                    <span class="skills__slider-item">HTML & CSS</span>
+                    <span class="skills__slider-item">Javascript & Typescript</span>
+                    <span class="skills__slider-item">WordPress</span>
+                    <span class="skills__slider-item">React</span>
+                    <span class="skills__slider-item">Laravel</span>
+                    <span class="skills__slider-item">NodeJS</span>
+                    <span class="skills__slider-item">GitHub</span>
+                    <span class="skills__slider-item">Flutter</span>
+                </div>
+            </div>
+
+            <div class="skills__control">
+                <div class="skills__control-arrow">
+                    <img
+                            loading="lazy"
+                            src="/content/images/icon-arrow-left-slider-black.svg"
+                            alt="Icône flèche vers la gauche"
+                            width="32"
+                            height="32">
+                </div>
+
+                <div class="skills__control-separator"></div>
+
+                <div class="skills__control-arrow">
+                    <img
+                            loading="lazy"
+                            src="/content/images/icon-arrow-right-slider-black.svg"
+                            alt="Icône flèche vers la droite"
+                            width="32"
+                            height="32">
+                </div>
+            </div>
+        </div>
+
+        <a class="skills__button skills__button--end" href="#">
+            <span class="skills__button-link">Mes compétences</span>
+        </a>
+    </div>
+</section>
