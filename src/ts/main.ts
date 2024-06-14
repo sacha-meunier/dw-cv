68ddc111f838f52360657ee3518e53f2c92ccc8d
Sacha Meunier <sacha.meunier@student.hepl.be>
2024-06-14 17:24:03 +0200

    Added rotating text

--- src/ts/main.ts
+++ src/ts/main.ts
@@ -1,1 +1,15 @@
-console.log('hello world');
+import { RotatingText } from "./RotatingText";
+import { settings } from "./settings";
+
+document.addEventListener("DOMContentLoaded", () => {
+    const rotatingWrapper = document.querySelector(settings.rotatingText.containerSelector) as HTMLElement;
+    if (rotatingWrapper) {
+        new RotatingText(
+            settings.rotatingText.containerSelector,
+            settings.rotatingText.wordsSelector,
+            settings.rotatingText.delay,
+            settings.rotatingText.animationDuration,
+            settings.rotatingText.distance
+        );
+    }
+});
---
+++ src/ts/settings.ts
@@ -0,0 +1,9 @@
+export const settings = {
+    rotatingText: {
+        containerSelector: ".rotating-txt",
+        wordsSelector: ".word",
+        delay: 3000, // Délai entre les rotations en millisecondes.
+        animationDuration: 500, // Durée de l'animation en millisecondes.
+        distance: 100 // Distance de déplacement en pourcentage.
+    }
+};
