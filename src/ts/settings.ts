1c94c37121e00ba9644052487236a1f38646bd06
Sacha Meunier <sacha.meunier@student.hepl.be>
2024-06-17 14:12:06 +0200

    Delete settings.ts

--- src/ts/settings.ts
+++
@@ -1,9 +0,0 @@
-export const settings = {
-    rotatingText: {
-        containerSelector: ".rotating-txt",
-        wordsSelector: ".word",
-        delay: 3000, // Délai entre les rotations en millisecondes.
-        animationDuration: 500, // Durée de l'animation en millisecondes.
-        distance: 100 // Distance de déplacement en pourcentage.
-    }
-};
