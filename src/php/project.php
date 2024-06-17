57d2a2083aef89ae716398eb1a198e1e11c7cee2
Sacha Meunier <sacha.meunier@student.hepl.be>
2024-06-17 14:07:36 +0200

    Update project.php

--- src/php/project.php
+++ src/php/project.php
@@ -1,187 +1,103 @@
-<section id="projects" class="projects-wrapper">
-    <div class="heading-small-wrapper [ animate-up-fade ]">
-        <h2 class="heading-small upper" id="projetsPro" role="heading" aria-level="2">Expériences
-            professionnelles</h2>
-    </div>
-    <article class="tilt [ interactable ]">
-        <div class="projects-card bg-cyan">
-            <h3 class="projects-card-logo [ animate-up-fade ]" role="heading" aria-level="3">
-                <img
-                    loading="lazy"
-                    src="https://assets.website-files.com/5d27763c46ba9e561dc5b972/623480311c9b4b3a31afecb2_options%202.svg"
-                    alt="love the sales"
-                    class=""
-                    width="295"
-                    height="50">
-            </h3>
-            <p class="projects-card-p [ animate-up-fade ] [ clr-white ]">
-                J'ai créé des visuels et photos uniques pour un bar, capturant son ambiance distinctive pour des
-                affiches mémorables.
-            </p>
-            <div class="projects-card-img-wrapper">
-                <img
-                        loading="lazy"
-                        src="./content/images/project-card-bg.svg"
-                        alt=""
-                        class="projects-img-bg">
-                <img
-                        loading="lazy"
-                        src="https://assets.website-files.com/5d27763c46ba9e561dc5b972/62347ffa0e6070c124d011f9_Group%20116045998%20(2).png"
-                        alt=""
-                        width="374"
-                        height="330.117"
-                        class="projects-img-1 [ animate-left-fade ]">
-                <img
-                        loading="lazy"
-                        src="https://assets.website-files.com/5d27763c46ba9e561dc5b972/62347fe33dffd3867e725707_Group%20116045997.png"
-                        alt=""
-                        width="282"
-                        height="169"
-                        class="projects-img-1-1 [ animate-left-fade ]">
+<section id="projects" class="projects">
+    <div class="projects-wrapper">
+        <div class="heading-small-wrapper" data-animate="slide-up-fade">
+            <h2 class="heading-small" id="projetsPro" role="heading" aria-level="2">Mes réalisations</h2>
+        </div>
+        <article class="tilt" data-action="trailer">
+            <div class="projects-card bg-cyan">
+                <h3 class="projects-card-title" data-animate="slide-up-fade" role="heading" aria-level="3">
+                    Logic Circles
+                </h3>
+                <p class="projects-card-p" data-animate="slide-up-fade">
+                    Logic Circle, le jeu de puzzle coloré qui va défier votre logique ! Mettez votre réflexion à
+                    l'épreuve et
+                    relevez le défi dès maintenant.
+                </p>
+                <div class="projects-card-img-wrapper">
+                    <!-- TODO IMG-->
+                </div>
+                <div class="card-highlight"></div>
+                <div class="projects-category-wrapper">
+                    <span class="projects-category" data-animate="slide-up-fade">Canvas</span>
+                    <span class="projects-category" data-animate="slide-up-fade">Games</span></div>
             </div>
-            <div class="card-highlight"></div>
-            <span class="projects-category [ animate-up-fade ] [ upper clr-white ]">Affiches</span>
-            <span class="projects-category [ animate-up-fade ] [ upper clr-white ]">Photos</span>
-        </div>
-        <div class="card-shadow"></div>
-    </article>
-
-    <!--<article class="tilt [ interactable ]">
-        <div class="projects-card bg-d-purple">
-            <h3 class="projects-card-logo [ animate-up-fade ]" role="heading" aria-level="3">
-                <img
-                    loading="lazy"
-                    src="https://assets.website-files.com/5d27763c46ba9e561dc5b972/623480311c9b4b3a31afecb2_options%202.svg"
-                    alt="love the sales"
-                    class=""
-                    width="295"
-                    height="50">
-            </h3>
-            <p class="projects-card-p [ animate-up-fade ] [ clr-white ]">
-                Projet bénévole pour une ASBL de la Salm. Rebranding, stratégies et mise en avant pour leur
-                permettre
-                d’aider plus de monde.
-            </p>
-            <img
-                loading="lazy"
-                src="https://assets.website-files.com/5d27763c46ba9e561dc5b972/62347ffa0e6070c124d011f9_Group%20116045998%20(2).png"
-                alt=""
-                width="374"
-                height="330.117"
-                class="projects-img-1 [ animate-left-fade ]">
-            <img
-                loading="lazy"
-                src="https://assets.website-files.com/5d27763c46ba9e561dc5b972/62347fe33dffd3867e725707_Group%20116045997.png"
-                alt=""
-                width="282"
-                height="169"
-                class="projects-img-1-1 [ animate-left-fade ]">
-            <div class="card-highlight"></div>
-            <span class="projects-category [ animate-up-fade ] [ upper clr-white ]">rebranding</span>
-            <span class="projects-category [ animate-up-fade ] [ upper clr-white ]">conversion</span>
-        </div>
-        <div class="card-shadow"></div>
-    </article>
-    <article class="tilt [ interactable ]">
-        <div class="projects-card bg-orange">
-            <h3 class="projects-card-logo [ animate-up-fade ]" role="heading" aria-level="3">
-                <img
-                    loading="lazy"
-                    src="https://assets.website-files.com/5d27763c46ba9e561dc5b972/623480311c9b4b3a31afecb2_options%202.svg"
-                    alt="love the sales"
-                    class=""
-                    width="295"
-                    height="50">
-            </h3>
-            <p class="projects-card-p [ animate-up-fade ] [ clr-white ]">
-                Projet bénévole pour une ASBL de la Salm. Rebranding, stratégies et mise en avant pour leur
-                permettre
-                d’aider plus de monde.
-            </p>
-            <img
-                loading="lazy"
-                src="https://assets.website-files.com/5d27763c46ba9e561dc5b972/62347ffa0e6070c124d011f9_Group%20116045998%20(2).png"
-                alt=""
-                width="374"
-                height="330.117"
-                class="projects-img-1 [ animate-left-fade ]">
-            <img
-                loading="lazy"
-                src="https://assets.website-files.com/5d27763c46ba9e561dc5b972/62347fe33dffd3867e725707_Group%20116045997.png"
-                alt=""
-                width="282"
-                height="169"
-                class="projects-img-1-1 [ animate-left-fade ]">
-            <div class="card-highlight"></div>
-            <span class="projects-category [ animate-up-fade ] [ upper clr-white ]">rebranding</span>
-            <span class="projects-category [ animate-up-fade ] [ upper clr-white ]">conversion</span>
-        </div>
-        <div class="card-shadow"></div>
-    </article>
-    <article class="tilt [ interactable ]">
-        <div class="projects-card bg-l-purple">
-            <h3 class="projects-card-logo [ animate-up-fade ]" role="heading" aria-level="3">
-                <img
-                    loading="lazy"
-                    src="https://assets.website-files.com/5d27763c46ba9e561dc5b972/623480311c9b4b3a31afecb2_options%202.svg"
-                    alt="love the sales"
-                    class=""
-                    width="295"
-                    height="50">
-            </h3>
-            <p class="projects-card-p [ animate-up-fade ] [ clr-white ]">
-                Projet bénévole pour une ASBL de la Salm. Rebranding, stratégies et mise en avant pour leur
-                permettre
-                d’aider plus de monde.
-            </p>
-            <img
-                loading="lazy"
-                src="https://assets.website-files.com/5d27763c46ba9e561dc5b972/62347ffa0e6070c124d011f9_Group%20116045998%20(2).png"
-                alt=""
-                width="374"
-                height="330.117"
-                class="projects-img-1 [ animate-left-fade ]">
-            <img
-                loading="lazy"
-                src="https://assets.website-files.com/5d27763c46ba9e561dc5b972/62347fe33dffd3867e725707_Group%20116045997.png"
-                alt=""
-                width="282"
-                height="169"
-                class="projects-img-1-1 [ animate-left-fade ]">
-            <div class="card-highlight"></div>
-            <span class="projects-category [ animate-up-fade ] [ upper clr-white ]">rebranding</span>
-            <span class="projects-category [ animate-up-fade ] [ upper clr-white ]">conversion</span>
-        </div>
-        <div class="card-shadow"></div>
-    </article>-->
-
-    <div class="projects-btn-wrapper [ animate-fade ]">
-        <a href="#"
-           class="btn projects-btn magnet [ bg-blue upper clr-white ][ hover-effect hover-white ][ interactable ]">
-            <div class="projects-btn-txt">Discutons</div>
-            <div class="projects-btn-img">
-                <img
-                    loading="lazy"
-                    src="/content/images/mail.svg"
-                    alt="Icône mail"
-                    width="18"
-                    height="18">
+            <div class="card-shadow"></div>
+        </article>
+        <article class="tilt" data-action="trailer">
+            <div class="projects-card bg-d-purple">
+                <h3 class="projects-card-title" data-animate="slide-up-fade" role="heading" aria-level="3">
+                    Portfolio
+                </h3>
+                <p class="projects-card-p" data-animate="slide-up-fade">
+                    Création d'un portfolio personnel utilisant les technologies HTML, SCSS, TypeScript et Wordpress
+                </p>
+                <div class="projects-card-img-wrapper">
+                    <!-- TODO IMG-->
+                </div>
+                <div class="card-highlight"></div>
+                <div class="projects-category-wrapper">
+                    <span class="projects-category" data-animate="slide-up-fade">Web</span>
+                    <span class="projects-category" data-animate="slide-up-fade">Wordpress</span></div>
             </div>
-            <div class="hover"></div>
-        </a>
-
-        <a href="#"
-           class="btn projects-btn-2 magnet [ bg-white clr-blue upper ] [ hover-effect hover-blue ][ interactable ]">
-            <div class="projects-btn-txt-2">Plus de projets</div>
-            <div class="projects-btn-img-2">
-                <img
-                    loading="lazy"
-                    src="/content/images/arrow-right.svg"
-                    alt="Icône flèche bleue"
-                    width="18"
-                    height="18">
+            <div class="card-shadow"></div>
+        </article>
+        <article class="tilt" data-action="trailer">
+            <div class="projects-card bg-orange">
+                <h3 class="projects-card-title" data-animate="slide-up-fade" role="heading" aria-level="3">
+                    Sef de Huy
+                </h3>
+                <p class="projects-card-p" data-animate="slide-up-fade">
+                    Projet bénévole pour une ASBL de Huy. Rebranding, stratégies et mise en avant de leurs services pour
+                    qu'ils aident plus de monde.
+                </p>
+                <div class="projects-card-img-wrapper">
+                    <!-- TODO IMG-->
+                </div>
+                <div class="card-highlight"></div>
+                <div class="projects-category-wrapper">
+                    <span class="projects-category" data-animate="slide-up-fade">Web</span>
+                    <span class="projects-category" data-animate="slide-up-fade">Rebranding</span>
+                    <span class="projects-category" data-animate="slide-up-fade">Wordpress</span>
+                </div>
             </div>
-            <div class="hover"></div>
-        </a>
+            <div class="card-shadow"></div>
+        </article>
+        <article class="tilt" data-action="trailer">
+            <div class="projects-card bg-l-purple">
+                <h3 class="projects-card-title" data-animate="slide-up" role="heading" aria-level="3">
+                    Le CV
+                </h3>
+                <p class="projects-card-p" data-animate="slide-up-fade">
+                    Recréation de la page d'accueil d'un site web existant et transformation de son contenu en notre cv.
+                </p>
+                <div class="projects-card-img-wrapper">
+                    <!-- TODO IMG -->
+                </div>
+                <div class="card-highlight"></div>
+                <div class="projects-category-wrapper">
+                    <span class="projects-category" data-animate="slide-up-fade">Web</span>
+                </div>
+            </div>
+            <div class="card-shadow"></div>
+        </article>
+
+        <div class="projects-btn-wrapper" data-animate="slide-up-fade">
+            <a class="contact__button" data-magnetic="true" data-action="trailer" href="#">
+                <span class="contact__button-text">Discutons</span>
+                <div class="contact__button-icon-wrapper"><img class="contact__button-icon"
+                                                               src="/content/images/icon-mail.svg" width="18"
+                                                               height="18"
+                                                               alt="Icône mail"></div>
+            </a>
+            <a class="contact__button contact__button--white" data-magnetic="true" data-action="trailer" href="#">
+                <span class="contact__button-text">Plus de projets</span>
+                <div class="contact__button-icon-wrapper"><img class="contact__button-icon"
+                                                               loading="lazy"
+                                                               src="/content/images/arrow-right.svg"
+                                                               alt="Icône flèche bleue"
+                                                               width="18"
+                                                               height="18"></div>
+            </a>
+        </div>
     </div>
 </section>
