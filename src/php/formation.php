ffce09173b4c9af58683ea1832d7ac5d1f07bc50
Sacha Meunier <sacha.meunier@student.hepl.be>
2024-06-17 14:07:25 +0200

    Create formation.php

---
+++ src/php/formation.php
@@ -0,0 +1,148 @@
+<section id="formation" class="formation-wrapper">
+    <div class="heading-middle-wrapper" data-animate="slide-up-fade">
+        <h2 class="heading-middle" role="heading" aria-level="2">Mes formations</h2>
+    </div>
+    <div class="formation-card-wrapper-holder">
+        <div class="formation-card-sticky">
+            <article class="formation-card-wrapper">
+                <div class="formation-heading-wrapper">
+                    <h3 class="formation-heading" role="heading" aria-level="3">Bachelier web
+                        développeur</h3>
+                    <div class="formation-banner-wrapper">
+                        <!--<img
+                                loading="lazy"
+                                class="formation-banner parallax"
+                                src="/content/images/formation-banner-1.png"
+                                alt="Bannière de l'équipe"
+                                width="469"
+                                height="200">-->
+                    </div>
+                </div>
+                <div class="formation-p-wrapper-1">
+                    <p class="formation-small-title">Développement web Front-End</p>
+                    <p class="formation-p-1">
+                        En tant que développeur front-end passionné, je m'efforce de créer des interfaces utilisateur
+                        modernes et intuitives qui captivent et engagent les utilisateurs.
+                    </p>
+                </div>
+                <div class="formation-p-wrapper-1">
+                    <p class="formation-small-title formation-small-title-2">Développement web
+                        Back-end</p>
+                    <p class="formation-p-1">
+                        En tant que développeur backend, je m'attache à concevoir et à implémenter des architectures
+                        robustes qui soutiennent efficacement les fonctionnalités essentielles des applications.
+                    </p>
+                </div>
+                <div class="formation-num-wrapper">
+                    <div class="formation-num-holder">
+                        <span class="formation-num">2021-2024</span>
+                        <!--<span class="formation-num-txt ">views</span>-->
+                    </div>
+                    <div class="formation-num-holder">
+                        <span class="formation-num">+15</span>
+                        <span class="formation-num-txt">projets</span>
+                    </div>
+                </div>
+                <div class="formation-divider"></div>
+            </article>
+        </div>
+        <div class="formation-card-sticky formation-card-sticky--blue">
+            <article class="formation-card-wrapper">
+                <div class="formation-heading-wrapper">
+                    <h3 class="formation-heading" role="heading" aria-level="3">
+                        formation personnelle
+                    </h3>
+                    <div class="formation-banner-wrapper">
+                        <!--<img
+                                loading="lazy"
+                                class="formation-banner"
+                                src="/content/images/formation-banner-1.png"
+                                alt="Bannière de l'équipe"
+                                width="469"
+                                height="200">-->
+                    </div>
+                </div>
+                <div class="formation-p-wrapper-2">
+                    <p class="formation-small-title">Auto-formation</p>
+                    <p class="formation-p-2">
+                        Devenir la meilleure version de sois est un objectif important pour moi, c'est pourquoi
+                        chaque jour j'apprends de nouvelles choses pour repousser mes limites.
+                    </p>
+                </div>
+                <!--<div class="formation-num-wrapper">
+                    <div class="formation-num-holder">
+                        <span class="formation-num">2-3x</span>
+                        <span class="formation-num-txt ">faster than internal design teams.</span>
+                    </div>
+                </div>-->
+                <div class="formation-p-wrapper-2">
+                    <p class="formation-small-title formation-small-title-2">Montage vidéo
+                    </p>
+                    <p class="formation-p-2">
+                        Depuis plusieurs années j'apprends le métier de monteur vidéo, afin de créer des vidéos qui
+                        suscitent l'attention des spectateurs du début à la fin
+                    </p>
+                </div>
+                <div class="formation-p-wrapper-2">
+                    <p class="formation-small-title formation-small-title-2">Création de contenu
+                    </p>
+                    <p class="formation-p-2">
+                        Depuis quelques années, j'étudie le fonctionnement des réseaux sociaux dans le but
+                        d'utiliser ses outils à des fins professionnelles.
+                    </p>
+                    <!--<p class="formation-p-2 formation-p-tablet">
+                        2020-2024
+                    </p>-->
+                </div>
+                <div class="formation-divider"></div>
+                    <div class="formation-num-wrapper">
+                        <div class="formation-num-holder">
+                            <span class="formation-num">2020-2024</span>
+                        </div>
+                    </div>
+            </article>
+        </div>
+        <div class="formation-card-sticky formation-card-sticky--green">
+            <article class="formation-card-wrapper">
+                <div class="formation-heading-wrapper">
+                    <h3 class="formation-heading" role="heading" aria-level="3">Bachelier technique
+                        graphique</h3>
+                    <div class="formation-banner-wrapper">
+                        <!--<img
+                                loading="lazy"
+                                class="formation-banner"
+                                src="/content/images/formation-banner-1.png"
+                                alt="Bannière de l'équipe"
+                                width="469"
+                                height="200">-->
+                    </div>
+                </div>
+                <div class="formation-p-wrapper-3">
+                    <p class="formation-small-title">Apprentissage du développement Web</p>
+                    <p class="formation-p-3">
+                        Ce fut le début de ma carrière en tant que web développeur, j'ai pu comprendre les enjeux de
+                        ce métier ainsi que les bases nécessaires.
+                    </p>
+                </div>
+                <div class="formation-p-wrapper-3">
+                    <p class="formation-small-title formation-small-title-2">Perfectionnement de mes
+                        designs
+                    </p>
+                    <p class="formation-p-3">
+                        Mes cours de graphisme m'ont permis de devenir excellent dans toutes les étapes de création
+                        de designs, liant typographies, couleurs et originalité.
+                    </p>
+                </div>
+                <div class="formation-p-wrapper-3">
+                    <p class="formation-small-title formation-small-title-2">Formation dans le domaine
+                        de la 3D.
+                    </p>
+                    <p class="formation-p-3">
+                        Grâce à mes cours de 3D, j'ai appris à créer des objects et des animations de hautes
+                        qualités.
+                    </p>
+                </div>
+            </article>
+        </div>
+    </div>
+</section>
