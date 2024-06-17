72aa11f5634048efc4ec37c9e3b26ee525aae926
Sacha Meunier <sacha.meunier@student.hepl.be>
2024-06-17 14:07:40 +0200

    Create testimonials.php

---
+++ src/php/testimonials.php
@@ -0,0 +1,163 @@
+<section id="testimonials" class="testimonials-wrapper">
+    <h2 class="heading testimonials-heading" data-animate="slide-up-fade" role="heading" aria-level="2">
+        <span class="testimonials-heading--blue">Avis</span><br>
+        de mes clients
+    </h2>
+    <div class="testimonials-articles-wrapper">
+        <article class="testimonials-article">
+            <h3 class="sro" role="heading" aria-level="3">Client 1</h3>
+            <div class="testimonials-img-holder-desktop" data-animate="slide-up-fade"><img
+                    loading="lazy"
+                    src="/content/images/testimonials/david-360.jpg"
+                    class="testimonials-pfp-desktop"
+                    alt="Photo de profil du clients"
+                    width="360"
+                    height="480"></div>
+            <div class="testimonials-profile">
+                <img
+                    loading="lazy"
+                    src="/content/images/testimonials/david-72.jpg"
+                    class="testimonials-pfp"
+                    data-animate="slide-up-fade"
+                    alt="Photo de profil du clients"
+                    width="72"
+                    height="72">
+                <div class="testimonials-holder" data-animate="fade-in">
+                    <p class="testimonials-name">Brice Maron</p>
+                    <div class="testimonials-roleImg">
+                        <p class="testimonials-role">Directeur de</p>
+                        <img
+                            class="testimonials-logo" loading="lazy"
+                            src="/content/images/testimonials/taverne.png"
+                            alt="Logo Taverne Waremme"
+                            width="108"
+                            height="36">
+                    </div>
+                </div>
+                <div class="testimonials-quote" data-animate="slide-up-fade">
+                    <img
+                        loading="lazy"
+                        class="testimonials-quote-img"
+                        src="/content/images/quote.svg"
+                        alt="Icône de paragraphe"
+                        width="180"
+                        height="180">
+                    <p class="testimonials-p">
+                        Toujours à l'heure et excellent en collaboration. Sa fiabilité est incontestable et il livre
+                        systématiquement des résultats de grande qualité.
+                </div>
+            </div>
+        </article>
+        <article class="testimonials-article">
+            <h3 class="sro" role="heading" aria-level="3">Client 2</h3>
+            <div class="testimonials-img-holder-desktop" data-animate="slide-up-fade"><img
+                    loading="lazy"
+                    src="/content/images/testimonials/pierre-360.jpg"
+                    class="testimonials-pfp-desktop"
+                    alt="Photo de profil du clients"
+                    width="360"
+                    height="480"></div>
+            <div class="testimonials-profile">
+                <img
+                    loading="lazy"
+                    src="/content/images/testimonials/pierre-72.jpg"
+                    class="testimonials-pfp"
+                    data-animate="slide-up-fade"
+                    alt="Photo de profil du clients"
+                    width="72"
+                    height="72">
+                <div class="testimonials-holder" data-animate="fade-in">
+                    <p class="testimonials-name">Pierre Kabwe</p>
+                    <div class="testimonials-roleImg">
+                        <p class="testimonials-role">Directeur de</p>
+                        <img
+                            class="testimonials-logo" loading="lazy"
+                            src="/content/images/testimonials/elle-sort.png"
+                            alt="Logo tinder"
+                            width="108"
+                            height="36">
+                    </div>
+                </div>
+                <div class="testimonials-quote" data-animate="slide-up-fade">
+                    <img
+                        loading="lazy"
+                        class="testimonials-quote-img"
+                        src="/content/images/quote.svg"
+                        alt="Icône de paragraphe"
+                        width="180"
+                        height="180">
+                    <p class="testimonials-p testimonials-p-26">
+                        Sacha a démontré une fiabilité et une rapidité remarquables. Nous avons travaillé pendant
+                        plus d'un an avec lui et il a toujours répondu à nos attentes. Il a su se démarquer de nos
+                        précédents graphistes et a toujours compris ce que nous recherchions. Il cherche toujours à
+                        offrir un
+                        résultat des plus parfaits.
+                </div>
+            </div>
+        </article>
+        <article class="testimonials-article">
+            <h3 class="sro" role="heading" aria-level="3">Client 3</h3>
+            <div class="testimonials-img-holder-desktop" data-animate="slide-up-fade"><img
+                    loading="lazy"
+                    src="/content/images/testimonials/harvey-360.jpg"
+                    class="testimonials-pfp-desktop"
+                    alt="Photo de profil du clients"
+                    width="360"
+                    height="360"></div>
+            <div class="testimonials-profile">
+                <img
+                    loading="lazy"
+                    src="/content/images/testimonials/harvey-72.jpg"
+                    class="testimonials-pfp"
+                    data-animate="slide-up-fade"
+                    alt="Photo de profil du clients"
+                    width="72"
+                    height="72">
+                <div class="testimonials-holder" data-animate="fade-in">
+                    <p class="testimonials-name">Lucien Lelièvre</p>
+                    <div class="testimonials-roleImg">
+                        <p class="testimonials-role">Directeur de</p>
+                        <img
+                            class="testimonials-logo" loading="lazy"
+                            src="/content/images/testimonials/koenigsegg.png"
+                            alt="Logo tinder"
+                            width="108"
+                            height="36">
+                    </div>
+                </div>
+                <div class="testimonials-quote" data-animate="slide-up-fade">
+                    <img
+                        loading="lazy"
+                        class="testimonials-quote-img"
+                        src="/content/images/quote.svg"
+                        alt="Icône de paragraphe"
+                        width="180"
+                        height="180">
+                    <p class="testimonials-p testimonials-p-22">
+                        Sacha est un développeur web exceptionnel. Il est très perfectionniste et fait toujours de
+                        son mieux pour fournir un travail de qualité. De plus, il est très rapide et peut terminer
+                        un projet en un temps records. Il prend également le temps de bien comprendre les besoins de
+                        ses clients. Je vous recommande de travailler avec lui pour vos projets web.
+                </div>
+            </div>
+        </article>
+    </div>
+    <div class="arrow-slider-wrapper" data-animate="fade-in">
+        <div class="arrow-slider" data-action="trailer">
+            <img
+                loading="lazy"
+                src="/content/images/arrow-left-slider.svg"
+                alt="Icône flèche vers la gauche"
+                width="32"
+                height="32">
+        </div>
+        <div class="arrow-slider arrow-2" data-action="trailer">
+            <img
+                loading="lazy"
+                src="/content/images/arrow-right-slider.svg"
+                alt="Icône flèche vers la droite"
+                width="32"
+                height="32">
+        </div>
+    </div>
+</section>
