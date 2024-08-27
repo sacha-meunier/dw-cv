381719d906c39f7d1ad19cbb305281bb75e41daa
Sacha Meunier <sacha.meunier@student.hepl.be>
2024-08-27 17:13:49 +0200

    :sparkles: Adding correct images

--- src/php/project.php
+++ src/php/project.php
@@ -5,89 +5,113 @@
         </div>
 
         <!-- Project 1 -->
-        <article class="tilt" data-action="trailer" aria-labelledby="project-1-title" aria-describedby="project-1-desc" itemscope itemtype="https://schema.org/CreativeWork">
-            <div class="projects-card bg-cyan">
-                <h3 id="project-1-title" class="projects-card-title" data-animate="slide-up-fade" role="heading" aria-level="3" itemprop="headline">
-                    Portfolio
-                </h3>
-                <p id="project-1-desc" class="projects-card-p" data-animate="slide-up-fade" itemprop="description">
-                    Création d'un portfolio personnel utilisant les technologies HTML, SCSS, TypeScript et Wordpress.
-                </p>
-                <div class="projects-card-img-wrapper" data-animate="slide-up-fade">
-                    <img src="../../content/images/logic-circles.png" alt="Image du jeu logic circles" itemprop="image">
+        <article class="tilt-wrapper" data-action="trailer" aria-labelledby="project-1-title" aria-describedby="project-1-desc" itemscope itemtype="https://schema.org/CreativeWork">
+            <a class="tilt" href="https://sacha-meunier.com">
+                <div class="projects-card bg-cyan">
+                    <h3 id="project-1-title" class="projects-card-title" data-animate="slide-up-fade" role="heading"
+                        aria-level="3" itemprop="headline">
+                        Portfolio
+                    </h3>
+                    <p id="project-1-desc" class="projects-card-p" data-animate="slide-up-fade" itemprop="description">
+                        Création d'un portfolio personnel utilisant les technologies HTML, SCSS, TypeScript et
+                        Wordpress.
+                    </p>
+                    <div class="projects-card-img-wrapper p1" data-animate="slide-up-fade">
+                        <img src="../../content/images/projects/p1-desktop-portfolio.png" width="419" height="369"
+                             alt="Image du Portfolio en version desktop" itemprop="image">
+                        <img src="../../content/images/projects/p1-mobile-portfolio.png" width="169" height="282"
+                             alt="Image du Portfolio en version mobile" itemprop="image">
+                    </div>
+                    <div class="card-highlight"></div>
+                    <div class="projects-category-wrapper">
+                        <span class="projects-category" data-animate="slide-up-fade" itemprop="genre">Web</span>
+                        <span class="projects-category" data-animate="slide-up-fade" itemprop="genre">Wordpress</span>
+                    </div>
                 </div>
-                <div class="card-highlight"></div>
-                <div class="projects-category-wrapper">
-                    <span class="projects-category" data-animate="slide-up-fade" itemprop="genre">Web</span>
-                    <span class="projects-category" data-animate="slide-up-fade" itemprop="genre">Wordpress</span>
-                </div>
-            </div>
-            <div class="card-shadow"></div>
+                <div class="card-shadow"></div>
+            </a>
         </article>
 
         <!-- Project 2 -->
-        <article class="tilt" data-action="trailer" aria-labelledby="project-2-title" aria-describedby="project-2-desc" itemscope itemtype="https://schema.org/CreativeWork">
-            <div class="projects-card bg-d-purple">
-                <h3 id="project-2-title" class="projects-card-title" data-animate="slide-up-fade" role="heading" aria-level="3" itemprop="headline">
-                    Le Sef de Huy
-                </h3>
-                <p id="project-2-desc" class="projects-card-p" data-animate="slide-up-fade" itemprop="description">
-                    Projet bénévole pour une ASBL de Huy. Rebranding, stratégies et mise en avant de leurs services pour qu'ils aident plus de monde.
-                </p>
-                <div class="projects-card-img-wrapper" data-animate="slide-up-fade">
-                    <img src="../../content/images/portfolio.png" alt="Image du Portfolio" itemprop="image">
+        <article class="tilt-wrapper" data-action="trailer" aria-labelledby="project-2-title" aria-describedby="project-2-desc" itemscope itemtype="https://schema.org/CreativeWork">
+            <a class="tilt" href="https://sef.sacha-meunier.com">
+                <div class="projects-card bg-d-purple">
+                    <h3 id="project-2-title" class="projects-card-title" data-animate="slide-up-fade" role="heading"
+                        aria-level="3" itemprop="headline">
+                        Le Sef de Huy
+                    </h3>
+                    <p id="project-2-desc" class="projects-card-p" data-animate="slide-up-fade" itemprop="description">
+                        Projet bénévole pour une ASBL de Huy. Rebranding, stratégies et mise en avant de leurs services
+                        pour qu'ils aident plus de monde.
+                    </p>
+                    <div class="projects-card-img-wrapper p2" data-animate="slide-up-fade">
+                        <img src="../../content/images/projects/p2-desktop-sef.png" width="620" height="381"
+                             alt="Image du projet SEF en version desktop" itemprop="image">
+                        <img src="../../content/images/projects/p2-mobile-sef.png" width="135" height="236"
+                             alt="Image du projet SEF en version mobile" itemprop="image">
+                    </div>
+                    <div class="card-highlight"></div>
+                    <div class="projects-category-wrapper">
+                        <span class="projects-category" data-animate="slide-up-fade" itemprop="genre">Web</span>
+                        <span class="projects-category" data-animate="slide-up-fade" itemprop="genre">Rebranding</span>
+                        <span class="projects-category" data-animate="slide-up-fade" itemprop="genre">Wordpress</span>
+                    </div>
                 </div>
-                <div class="card-highlight"></div>
-                <div class="projects-category-wrapper">
-                    <span class="projects-category" data-animate="slide-up-fade" itemprop="genre">Web</span>
-                    <span class="projects-category" data-animate="slide-up-fade" itemprop="genre">Rebranding</span>
-                    <span class="projects-category" data-animate="slide-up-fade" itemprop="genre">Wordpress</span>
-                </div>
-            </div>
-            <div class="card-shadow"></div>
+                <div class="card-shadow"></div>
+            </a>
         </article>
 
         <!-- Project 3 -->
-        <article class="tilt" data-action="trailer" aria-labelledby="project-3-title" aria-describedby="project-3-desc" itemscope itemtype="https://schema.org/CreativeWork">
-            <div class="projects-card bg-orange">
-                <h3 id="project-3-title" class="projects-card-title" data-animate="slide-up-fade" role="heading" aria-level="3" itemprop="headline">
-                    Le CV
-                </h3>
-                <p id="project-3-desc" class="projects-card-p" data-animate="slide-up-fade" itemprop="description">
-                    Recréation de la page d'accueil d'un site web existant et transformation de son contenu en notre CV.
-                </p>
-                <div class="projects-card-img-wrapper" data-animate="slide-up-fade">
-                    <img src="../../content/images/orizon.png" alt="Image du CV" itemprop="image">
+        <article class="tilt-wrapper" data-action="trailer" aria-labelledby="project-3-title" aria-describedby="project-3-desc" itemscope itemtype="https://schema.org/CreativeWork">
+            <a class="tilt" href="#">
+                <div class="projects-card bg-orange">
+                    <h3 id="project-3-title" class="projects-card-title" data-animate="slide-up-fade" role="heading"
+                        aria-level="3" itemprop="headline">
+                        Le CV
+                    </h3>
+                    <p id="project-3-desc" class="projects-card-p" data-animate="slide-up-fade" itemprop="description">
+                        Recréation de la page d'accueil d'un site web existant et transformation de son contenu en notre
+                        CV.
+                    </p>
+                    <div class="projects-card-img-wrapper p3" data-animate="slide-up-fade">
+                        <img src="../../content/images/projects/p3-mobile-cv.png" width="575" height="781"
+                             alt="Image du CV officiel en version mobile" itemprop="image">
+                    </div>
+                    <div class="card-highlight"></div>
+                    <div class="projects-category-wrapper">
+                        <span class="projects-category" data-animate="slide-up-fade" itemprop="genre">Web</span>
+                        <span class="projects-category" data-animate="slide-up-fade" itemprop="genre">Static</span>
+                    </div>
                 </div>
-                <div class="card-highlight"></div>
-                <div class="projects-category-wrapper">
-                    <span class="projects-category" data-animate="slide-up-fade" itemprop="genre">Web</span>
-                    <span class="projects-category" data-animate="slide-up-fade" itemprop="genre">Static</span>
-                </div>
-            </div>
-            <div class="card-shadow"></div>
+                <div class="card-shadow"></div>
+            </a>
         </article>
 
         <!-- Project 4 -->
-        <article class="tilt" data-action="trailer" aria-labelledby="project-4-title" aria-describedby="project-4-desc" itemscope itemtype="https://schema.org/CreativeWork">
-            <div class="projects-card bg-l-purple">
-                <h3 id="project-4-title" class="projects-card-title" data-animate="slide-up" role="heading" aria-level="3" itemprop="headline">
-                    Logic Circles
-                </h3>
-                <p id="project-4-desc" class="projects-card-p" data-animate="slide-up-fade" itemprop="description">
-                    Logic Circle, le jeu de puzzle coloré qui va défier votre logique ! Mettez votre réflexion à l'épreuve et relevez le défi dès maintenant.
-                </p>
-                <div class="projects-card-img-wrapper" data-animate="slide-up-fade">
-                    <img src="../../content/images/sef.png" alt="Image du Sef de Huy" itemprop="image">
+        <article class="tilt-wrapper" data-action="trailer" aria-labelledby="project-4-title" aria-describedby="project-4-desc" itemscope itemtype="https://schema.org/CreativeWork">
+            <a class="tilt" href="https://expert-adventure-37wg5y8.pages.github.io/">
+                <div class="projects-card bg-l-purple">
+                    <h3 id="project-4-title" class="projects-card-title" data-animate="slide-up" role="heading"
+                        aria-level="3" itemprop="headline">
+                        Logic Circles
+                    </h3>
+                    <p id="project-4-desc" class="projects-card-p" data-animate="slide-up-fade" itemprop="description">
+                        Logic Circle, le jeu de puzzle coloré qui va défier votre logique ! Mettez votre réflexion à
+                        l'épreuve et relevez le défi dès maintenant.
+                    </p>
+                    <div class="projects-card-img-wrapper p4" data-animate="slide-up-fade">
+                        <img src="../../content/images/projects/p4-desktop-logic-circle.png" width="487" height="502"
+                             alt="Image du jeu logic circles en version desktop" itemprop="image">
+                    </div>
+                    <div class="card-highlight"></div>
+                    <div class="projects-category-wrapper">
+                        <span class="projects-category" data-animate="slide-up-fade" itemprop="genre">Web</span>
+                        <span class="projects-category" data-animate="slide-up-fade" itemprop="genre">Games</span>
+                        <span class="projects-category" data-animate="slide-up-fade" itemprop="genre">Canvas</span>
+                    </div>
                 </div>
-                <div class="card-highlight"></div>
-                <div class="projects-category-wrapper">
-                    <span class="projects-category" data-animate="slide-up-fade" itemprop="genre">Web</span>
-                    <span class="projects-category" data-animate="slide-up-fade" itemprop="genre">Games</span>
-                    <span class="projects-category" data-animate="slide-up-fade" itemprop="genre">Canvas</span>
-                </div>
-            </div>
-            <div class="card-shadow"></div>
+                <div class="card-shadow"></div>
+            </a>
         </article>
 
         <div class="projects-btn-wrapper" data-animate="slide-up-fade">
